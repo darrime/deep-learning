768c1c5fa961b34ef291bd71dd04a54f7d0c4f3b
darrime <darrime627@gmail.com>
2021-07-29 20:24:32 +0900

    3층 신경망 구성

--- main.py
+++ main.py
@@ -1,39 +1,76 @@
 import numpy as np
+import matplotlib.pylab as plt
+
 
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.7
-    tmp = np.sum(w*x) + b
+    tmp = np.sum(w * x) + b
     if tmp <= 0:
         return 0
     elif tmp > 0:
         return 1
 
+
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
     b = 0.7
-    tmp = np.sum(x*w) + b
+    tmp = np.sum(x * w) + b
     if tmp <= 0:
         return 0
     else:
         return
 
+
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.2
-    tmp = np.sum(x*w) + b
+    tmp = np.sum(x * w) + b
     if tmp <= 0:
         return 0
     else:
         return 1
+
+
 def XOR(x1, x2):
     s1 = NAND(x1, x2)
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
-def step_funtion(x):
-    y = x > 0
-    return y.astype(np.int)
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def identity_func(x):
+    return x
+
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1)
+
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A1 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A1)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_func(A3)
+print(Y)
